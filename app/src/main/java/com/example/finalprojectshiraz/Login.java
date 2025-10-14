53885360b0965722ba4396829f9622641fb29376
class01 <shirazhamoud0@gmail.com>
2025-10-14 11:47:04 +0300

    fff

--- app/src/main/java/com/example/finalprojectshiraz/Login.java
+++ app/src/main/java/com/example/finalprojectshiraz/Login.java
@@ -1,6 +1,8 @@
 package com.example.finalprojectshiraz;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
 
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+        usernameEditText = findViewById(R.id.editTextText);
+        passwordEditText = findViewById(R.id.editTextTextPassword);
+        loginButton = findViewById(R.id.button);
+        loginButton.setOnClickListener(v -> {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
