f51e8258b3bdcaa69fb8b4da7d92aa9e2b199ede
class01 <shirazhamoud0@gmail.com>
2025-11-04 11:39:07 +0200

    fffuuhhtgfb nbjhrgrtgghg

--- app/src/main/java/com/example/finalprojectshiraz/LogIn.java
+++ app/src/main/java/com/example/finalprojectshiraz/LogIn.java
@@ -1,6 +1,11 @@
 package com.example.finalprojectshiraz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +14,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LogIn extends AppCompatActivity {
-
+private TextView tvLogin;
+private TextView tvText;
+private TextView tvEmailAddress;
+private EditText etEnterEmail;
+private TextView tvPassword;
+private TextView tvEnterPass;
+private TextView tvF;
+private Button btnLogin;
+private Button btnSignUP;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_in);
@@ -20,5 +37,30 @@ public class LogIn extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        tvLogin = findViewById(R.id.tvLogin);
+        tvText = findViewById(R.id.tvText);
+        tvEmailAddress = findViewById(R.id.tvEmailAddress);
+        etEnterEmail = findViewById(R.id.etEnterEmail);
+        tvPassword = findViewById(R.id.tvPassword);
+        tvEnterPass = findViewById(R.id.tvEnterPass);
+        tvF = findViewById(R.id.tvF);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUP = findViewById(R.id.btnSignUP);
+
+        btnSignUP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogIn.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogIn.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
