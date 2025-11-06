8d82902babe2e7de60f3f8f74fed51db3400893e
class01 <shirazhamoud0@gmail.com>
2025-11-06 13:41:15 +0200

    jhgbyjg

--- app/src/main/java/com/example/finalprojectshiraz/AnimalDetails.java
+++ app/src/main/java/com/example/finalprojectshiraz/AnimalDetails.java
@@ -1,6 +1,8 @@
 package com.example.finalprojectshiraz;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class AnimalDetails extends AppCompatActivity {
+    private Button btnSubmit;
+    private TextInputLayout tilName;
+    private TextInputLayout tilAge;
+    private TextInputLayout tilGender;
+    private TextInputLayout tilBreed;
+    private TextInputLayout tilColor;
+    private TextInputLayout tilVaccineDetails;
+    private TextInputLayout tilNotes;
+    private CheckBox cbVaccinated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/finalprojectshiraz/HomeScreen.java
+++ app/src/main/java/com/example/finalprojectshiraz/HomeScreen.java
@@ -1,6 +1,10 @@
 package com.example.finalprojectshiraz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomeScreen extends AppCompatActivity {
+    private Button btn3;
+    private TextView tvH;
+    private TextView tv11;
+    private TextView tv13;
+    private TextView tv14;
+    private TextView tvLocation;
+    private TextView tv16;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,20 @@ public class HomeScreen extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btn3 = findViewById(R.id.btn3);
+        tvH = findViewById(R.id.tvH);
+        tv11 = findViewById(R.id.tv11);
+        tv13 = findViewById(R.id.tv13);
+        tv14 = findViewById(R.id.tv14);
+        tvLocation = findViewById(R.id.tvLocation);
+        tv16 = findViewById(R.id.tv16);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeScreen.this, AnimalDetails.class);
+                startActivity(intent);
+            }
         });
     }
 }
