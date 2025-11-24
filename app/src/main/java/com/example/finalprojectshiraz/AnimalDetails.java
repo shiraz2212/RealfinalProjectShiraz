acf3ae2e0e34c58c1bd18f0819f57490e4c53e50
class01 <shirazhamoud0@gmail.com>
2025-11-24 08:52:50 +0200

    jkujhjjuijkiiujijkjkrlkltreoi.

--- app/src/main/java/com/example/finalprojectshiraz/AnimalDetails.java
+++ app/src/main/java/com/example/finalprojectshiraz/AnimalDetails.java
@@ -30,10 +30,10 @@ public class AnimalDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_animal_details);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
