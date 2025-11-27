8da9c96448025909acfb263eb97d6b81cf8bc0da
class01 <shirazhamoud0@gmail.com>
2025-11-27 14:09:09 +0200

    jkujhjjuijkiiujijkjkrlkltreoi.

--- app/src/main/java/com/example/finalprojectshiraz/data/AnimalTable/MyAnimalAdapter.java
+++ app/src/main/java/com/example/finalprojectshiraz/data/AnimalTable/MyAnimalAdapter.java
@@ -1,10 +1,17 @@
 package com.example.finalprojectshiraz.data.AnimalTable;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.example.finalprojectshiraz.R;
 
 public class MyAnimalAdapter extends ArrayAdapter<Animal>
 {
@@ -33,8 +40,29 @@ public class MyAnimalAdapter extends ArrayAdapter<Animal>
      */
 
     @NonNull
-    @Override
+
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        //בניית הפריט הגרפי מתו קובץ העיצוב
+        View vitem= convertView;
+        if(vitem==null)
+            vitem= LayoutInflater.from(getContext()).inflate(itemLayout,parent,false);
+        //קבלת הפניות לרכיבים בקובץ העיצוב
+        ImageView imageView=vitem.findViewById(R.id.imgVitm);
+        TextView tvItemTitle=vitem.findViewById(R.id.tvItemTitle);
+        TextView tvItemText=vitem.findViewById(R.id.tvItemText);
+        TextView tvItemImportance=vitem.findViewById(R.id.tvItemImportance);
+        ImageButton imgBtnSendSms=vitem.findViewById(R.id.imgBtnSendSmsitm);
+        ImageButton imgBtnCall=vitem.findViewById(R.id.imgBtnCallitm);
+        ImageButton imgBtnEdit=vitem.findViewById(R.id.imgBtnEdititm);
+        ImageButton imgBtnDelete=vitem.findViewById(R.id.imgBtnDeleteitm);
+        //קבלת הנתון (עצם) הנוכחי
+        Animal current=getItem(position);
+
+
+
+
+
+        return vitem;
     }
+
 }
